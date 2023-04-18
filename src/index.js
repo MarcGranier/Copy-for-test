373bfc710c38ebfa808f4d552f4ba259dbd2b3b4
Marc Granier <mgranier@comcast.net>
2023-04-18 14:03:08 -0400

    Comment in index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+// random comment
 
 import { ProductsProvider } from './context/products_context'
 import { FilterProvider } from './context/filter_context'
